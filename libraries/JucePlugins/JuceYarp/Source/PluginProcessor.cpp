2ea42fa406791414b1434b729ec4e25bf8706805
jgvictores <jcgvicto@gmail.com>
2019-03-31 14:26:24 +0200

    add code to get state
    - https://docs.juce.com/master/tutorial_audio_processor_value_tree_state.html

--- libraries/JucePlugins/JuceYarp/Source/PluginProcessor.cpp
+++ libraries/JucePlugins/JuceYarp/Source/PluginProcessor.cpp
@@ -202,6 +202,9 @@ void JuceYarpAudioProcessor::getStateInformation (MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    auto state = parameters.copyState();
+    std::unique_ptr<XmlElement> xml (state.createXml());
+    copyXmlToBinary (*xml, destData);
 }
 
 void JuceYarpAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
